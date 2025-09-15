fda12b66cd6d6e6fbcea5ffdf692d4bd12378873
menajkinila16-code <menajkinila16@gmail.com>
2025-09-15 12:19:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ведите конечное число ");
+double x = Convert.ToDouble(Console.ReadLine());
+double i = 1;
+double S = 0;
+while (i <= x)
+{
+    i++;
+    S += i / (i++);
+
+}
+Console.WriteLine(S);
